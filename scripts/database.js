0fcf2721fadf15eb77b31c450fa6ba462b5cdf80
JoyceRuobingZhang <joyceruobingzhang@gmail.com>
2021-04-23 21:23:34 -0500

    added js files to automate fish

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,31 @@
-const database = {
-    fish: [{
+export const getFishDatabase = () => {
+    return [{
             name: "Mr. Tickles",
-            size: 2,
+            species: "gold fish",
+            length: "2 inches",
+            location: "Mexico",
             food: "Kale chips",
+            image: ["../images/mr.tickles.jpg", "gold fish"]
         },
         {
-
+            name: "Dr. Blubz",
+            species: "Piranha",
+            length: "7 inches",
+            location: "Brazil",
+            food: "gold fish",
+            image: ["../images/Piranha.png", "Piranha"]
+        },
+        {
+            name: "Red Jackie",
+            species: "Siamese Fighting Fish",
+            length: "2 inches",
+            location: "Cambodia",
+            food: "Fish Flakes",
+            image: ["../images/red jackie.jpg", "Red Jackie"]
         }
     ]
 }
+
+// function fishArray() {
+//     return database[fish].slice();
+// }
